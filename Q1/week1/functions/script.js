f41c0f91a8019c3995c6fd14da85e1ac53d5e7af
Jen Shannon <jennifer.shannon@galvanize.com>
2017-08-24 10:55:28 -0400

    Add some invocation

--- Q1/week1/functions/script.js
+++ Q1/week1/functions/script.js
@@ -1,3 +1,9 @@
-function myFunc(){
-  console.log("Your machine is infected!!!")
+
+
+function personalVirus(myName, yourName){
+
+  console.log("Your machine is infected, " + myName + "!!!");
+  console.log("Your machine is infected, " + yourName + "!!!");
 }
+
+personalVirus("Jen", "Steve");

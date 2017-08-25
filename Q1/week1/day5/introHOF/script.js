9089cb46163435db295cf5f00fc31871ed305a9a
Jen Shannon <jennifer.shannon@galvanize.com>
2017-08-25 10:53:42 -0400

    Add hof

---
+++ Q1/week1/day5/introHOF/script.js
@@ -0,0 +1,24 @@
+setInterval(function(){alert("HI")}, 3000)
+
+
+
+
+
+
+
+
+
+
+
+
+// myFunc();
+//
+// function myFunc(){
+//   console.log("Good morning g65!");
+// }
+
+
+
+// var myFunc2 = function(){
+//   console.log("TGIF")
+// }

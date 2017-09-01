475c5713526117738cf70d22560266d6d49795cd
Jen Shannon <jennifer.shannon@galvanize.com>
2017-09-01 13:45:51 -0400

    Add heavy shit

--- Q1/week2/events/script.js
+++ Q1/week2/events/script.js
@@ -1,26 +1,32 @@
-
-var p1 = document.querySelector('#par1');
-var p2 = document.querySelector('#par2');
-var p3 = document.querySelector('#par3');
-
-p1.addEventListener('click', function(){
-  console.log("You have clicked par1")
-})
-
-p2.addEventListener('click', function(){
-  console.log("You have clicked par2")
-})
-
-p3.addEventListener('click', function(){
-  console.log("You have clicked par3")
-})
-
-
+//
+// var p1 = document.querySelector('#par1');
+// var p2 = document.querySelector('#par2');
+// var p3 = document.querySelector('#par3');
+//
+// p1.addEventListener('click', function(event){
+//   console.log(event)
+// })
+//
+// p2.addEventListener('click', function(event){
+//   console.log(pars[i])
+// })
+//
+// p3.addEventListener('click', function(event){
+//   console.log(event.target.textContent)
+// })
 
 
 
 
 
+var pars = document.querySelectorAll('p');
+
+
+for (var i = 0; i<pars.length; i++) {
+  pars[i].addEventListener("click", function(event){
+    console.log(event.target.textContent);
+  })
+}
 
 
 

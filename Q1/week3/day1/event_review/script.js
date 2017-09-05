2418106a3d6163e1c0738a9f760fec607beeb375
Jen Shannon <jennifer.shannon@galvanize.com>
2017-09-05 11:24:28 -0400

    add class

--- Q1/week3/day1/event_review/script.js
+++ Q1/week3/day1/event_review/script.js
@@ -1,19 +1,19 @@
-document.querySelector('#red').addEventListener("click", function(){
+document.querySelector('.red').addEventListener("click", function(){
   //change the background color of the body
   document.querySelector('body').style.backgroundColor = "red";
 })
 
-document.querySelector('#blue').addEventListener("click", function(){
+document.querySelector('.blue').addEventListener("click", function(){
   //change the background color of the body
   document.querySelector('body').style.backgroundColor = "blue";
 })
 
-document.querySelector('#yellow').addEventListener("click", function(){
+document.querySelector('.yellow').addEventListener("click", function(){
   //change the background color of the body
   document.querySelector('body').style.backgroundColor = "yellow";
 })
 
-document.querySelector('#green').addEventListener("click", function(){
+document.querySelector('.green').addEventListener("click", function(){
   //change the background color of the body
   document.querySelector('body').style.backgroundColor = "green";
 })

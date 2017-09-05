d37577b38a4d82bca838ae5418f4a70aa8341fce
Jen Shannon <jennifer.shannon@galvanize.com>
2017-09-05 11:07:40 -0400

    Add box ex

---
+++ Q1/week2/day1/hofs/script.js
@@ -0,0 +1,16 @@
+// function awesome(monkey, num){
+//   console.log("This is why, Josh")
+//   var myNum = num;
+//   console.log(myNum)
+//   monkey();
+// }
+//
+// var myFunc = function(){
+//   alert("You've got mail")
+// }
+//
+// awesome(myFunc, 45);
+
+setInterval(function(){
+  console.log("I believe in you!")
+},3000)
---
+++ Q1/week3/day1/event_review/script.js
@@ -0,0 +1,19 @@
+document.querySelector('#red').addEventListener("click", function(){
+  //change the background color of the body
+  document.querySelector('body').style.backgroundColor = "red";
+})
+
+document.querySelector('#blue').addEventListener("click", function(){
+  //change the background color of the body
+  document.querySelector('body').style.backgroundColor = "blue";
+})
+
+document.querySelector('#yellow').addEventListener("click", function(){
+  //change the background color of the body
+  document.querySelector('body').style.backgroundColor = "yellow";
+})
+
+document.querySelector('#green').addEventListener("click", function(){
+  //change the background color of the body
+  document.querySelector('body').style.backgroundColor = "green";
+})

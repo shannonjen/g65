389f897c451710dceae0214235447d02ecd37a96
Jen Shannon <jennifer.shannon@galvanize.com>
2017-09-11 11:06:58 -0400

    Add some fun

--- Q1/week4/pokemon_app/script.js
+++ Q1/week4/pokemon_app/script.js
@@ -3,8 +3,8 @@ var searchForm = document.querySelector('#pokeSearch');
 searchForm.addEventListener("submit", function(event){
   event.preventDefault();
   var searchTerm = document.querySelector('input').value;
-  $.get('https://pokeapi.co/api/v2/pokemon/'+searchTerm,function(data){
-    console.log(data)
+  $.get('https://pokeapi.co/api/v2/pokemon/'+searchTerm +"/",function(data){
+    document.querySelector('#pokeName').innerText = data.forms[0].name;
   })
   // console.log(searchTerm);
 })

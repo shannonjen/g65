318be5c41af5892e4a50dfaad5ed5aae0381bf28
Jen Shannon <jennifer.shannon@galvanize.com>
2017-09-15 16:50:20 -0400

    Add json ajax

--- Q1/week4/ajax_json/script.js
+++ Q1/week4/ajax_json/script.js
@@ -1,15 +1,22 @@
-var students = {
-  "peeps": [{
-    "firstName": "Josh",
-    "favFood": "Coffee Pudding"
-  },{
-    "firstName": "Matt",
-    "favFood": "Birds"
-  },{
-    "firstName": "Romeo",
-    "favFood": "Salad"
+// var students = {
+//   "peeps": [{
+//     "firstName": "Josh",
+//     "favFood": "Coffee Pudding"
+//   },{
+//     "firstName": "Matt",
+//     "favFood": "Birds"
+//   },{
+//     "firstName": "Romeo",
+//     "favFood": "Salad"
+//   }
+//   ]
+// }
+$.getJSON("students.json",function(data){
+  console.log(data);
+  var peeples = data.peeps;
+  var result = "";
+  for (var i = 0; i<peeples.length; i++){
+    result = result + "<div><ul><li>"+peeples[i].firstName+"</li><li>"+peeples[i].favFood+"</li></ul></div>"
   }
-  ]
-}
-
-console.log(students.peeps[0].firstName);
+  document.querySelector("#main").innerHTML = result;
+})

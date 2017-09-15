55d1ac466720f233f07262d391882d101ad91d03
Jen Shannon <jennifer.shannon@galvanize.com>
2017-09-15 16:15:28 -0400

    Add class example prompt

---
+++ Q1/week4/ajax_json/script.js
@@ -0,0 +1,15 @@
+var students = {
+  "peeps": [{
+    "firstName": "Josh",
+    "favFood": "Coffee Pudding"
+  },{
+    "firstName": "Matt",
+    "favFood": "Birds"
+  },{
+    "firstName": "Romeo",
+    "favFood": "Salad"
+  }
+  ]
+}
+
+console.log(students.peeps[0].firstName);
